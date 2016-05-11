f61866f26e18995d455d0aa910b86a9ecdc911d8
Francis Bongiovanni <francisjbongiovanni@github.com>
2016-05-10 23:05:43 -0400

    loop into method

--- script.py
+++ script.py
@@ -8,29 +8,27 @@ with open('all-companies.json') as data_file:
 # amount of companies in Job Jawn
 amount_companies = len(data)
 
-skills = []
+technologies = []
 types = []
 titles = []
 title_counter = 0
 
-# Appends each technology to the skills array
-for companies in data:
-    for i in companies["technologies"]:
-        skills.append(i)
 
+def json_loop(key, array):
+    for companies in data:
+        for i in companies[key]:
+            array.append(i)
 
-for companies in data:
-    for i in companies["type"]:
-        types.append(i)
+json_loop("technologies", technologies)
+json_loop("type", types)
 
 for positions in data:
     for i in positions["positions"]:
         titles.append(i["title"])
         title_counter = title_counter + 1
 
+
 # Organizes data into JSON
-
-
 def json_formatter(json_name):
     # Organizes JSON as Key, Val
     formatted = ({i: json_name.count(i) for i in json_name})
@@ -42,6 +40,5 @@ def json_formatter(json_name):
     print formatted
     return
 
-json_formatter(skills)
+json_formatter(technologies)
 json_formatter(types)
-json_formatter(titles)
