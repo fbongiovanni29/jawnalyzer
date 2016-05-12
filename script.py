6f573b22b6e3a9610b683c804864cfe69b10916f
Francis Bongiovanni <francisjbongiovanni@github.com>
2016-05-11 23:33:59 -0400

    counties added & refactored

--- script.py
+++ script.py
@@ -1,33 +1,24 @@
 import json
+import glob
 from collections import OrderedDict
-
-# loads json and sets it to var data
-with open('all-companies.json') as data_file:
-    data = json.load(data_file)
 
 technologies = []
 types = []
 titles = []
 jobLocation = []
 employmentType = []
-my_json = []
-
-# prints amount of remote positions
-remote = json.dumps(data).count('"remote": true,')
-# amount of companies in Job Jawn
-amount_companies = len(data)
+job_json = []
 
 
+# Assigns Key: Val and appends to array
 def json_int(key, val):
     aDict = {}
     aDict[key] = val
-    my_json.append(aDict)
-
-json_int("remote", remote)
-json_int("companies", amount_companies)
+    job_json.append(aDict)
 
 
-def json_loop(key, array, position, my_json):
+# Extracts necessary JSON data and formats properly
+def json_loop(key, array, position, job_json):
     # Loops through json
     for companies in data:
         for i in companies[key]:
@@ -41,16 +32,28 @@ def json_loop(key, array, position, my_json):
     # Orders by Value descending
     formatted = sorted(formatted.items(), key=lambda x: x[1], reverse=True)
     formatted = OrderedDict(formatted)
-    # Converts back to JSON
+    # if nested inside of positions
     if key is "positions":
         key = position
     json_int(key, formatted)
     return
 
-json_loop("technologies", technologies, None, my_json)
-json_loop("type", types, None, my_json)
-json_loop("positions", jobLocation, "jobLocation", my_json)
-json_loop("positions", titles, "title", my_json)
-json_loop("positions", employmentType, "employmentType", my_json)
-with open('data.json', 'w') as outfile:
-    json.dump(my_json, outfile, indent=4)
+# loads json and sets it to var data
+for filename in glob.iglob('json/*'):
+    with open(filename) as data_file:
+        data = json.load(data_file)
+
+    # prints amount of remote positions
+    remote = json.dumps(data).count('"remote": true,')
+    # amount of companies in Job Jawn
+    amount_companies = len(data)
+
+    json_loop("technologies", technologies, None, job_json)
+    json_loop("type", types, None, job_json)
+    json_loop("positions", jobLocation, "jobLocation", job_json)
+    json_loop("positions", titles, "title", job_json)
+    json_loop("positions", employmentType, "employmentType", job_json)
+    json_int("remote", remote)
+    json_int("companies", amount_companies)
+    with open("analyzed/" + filename[+5:], 'w') as outfile:
+        json.dump(job_json, outfile, indent=4)
